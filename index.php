24f79c33253205f5509a99dfcc5a7027d756cea0
Kai <kai.hollberg@googlemail.com>
2013-10-19 12:22:23 +0200

    near empty template created

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>WMD Buchungssystem</title>
+</head>
+<body>
+	<header></header>
+	<section></section>
+	<footer></footer>
+</body>
+</html>
